bd42b37d6d871777e9e89e49a659094702dccf01
Micah Grafton <mgrafto1@uncc.edu>
2023-09-21 12:13:51 -0400

    feat:added implementation of the sandwich_maker functions, check_resources, make_sandwich, and init

--- sandwich_maker.py
+++ sandwich_maker.py
@@ -1,10 +1,19 @@
 class SandwichMaker:
+
     def __init__(self, resources):
-        self.machine_resources = resources
+        self.resources = resources
 
+    # Takes ingredients as a attribute and returns true if the order can be made and false if ingredients are
+    # insufficient
     def check_resources(self, ingredients):
-        """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        for ingredient, quantity in ingredients.items():
+            if self.resources.get(ingredient) is None or self.resources[ingredient] < quantity:
+                return False
+        return True
 
+    # Takes sandwich_size and order_ingredients as attributes
+    # and subtracts the resources used from the resources dictionary
+    # has no return type
     def make_sandwich(self, sandwich_size, order_ingredients):
-        #######
+        for ingredient, quantity in order_ingredients.items():
+            self.resources[ingredient] -= quantity
