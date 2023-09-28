e046997dc2083fa878feb458eb24008d042f78cb
Micah Grafton <mgrafto1@uncc.edu>
2023-09-28 16:20:38 -0400

    feat: add imports and api function from part 1

--- Assignment4/main4.py
+++ Assignment4/main4.py
@@ -1,8 +1,10 @@
+# add all code from part one of assignemnt 4
 from typing import Union
 
 from fastapi import FastAPI
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
@@ -10,5 +12,5 @@ def read_root():
 
 
 @app.get("/items/{item_id")
-def read_item(item_id: int, q: Union[str,None] = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
