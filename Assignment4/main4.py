1ff566e83bf8343e57dde5dbaee1fb694ee025ed
Micah Grafton <mgrafto1@uncc.edu>
2023-10-12 15:22:51 -0400

    feat: add imports and api functions from part 1, steps 1-5.

--- Assignment4/main4.py
+++ Assignment4/main4.py
@@ -5,12 +5,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
+#Method 1 from part 1
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
+#Method 2 from part 2
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
