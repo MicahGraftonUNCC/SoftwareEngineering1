6c3868422a4c1c40be636870c4bef331d0dae0b3
Micah Grafton <mgrafto1@uncc.edu>
2023-10-26 21:19:05 -0400

    feat: added end points for recipe table

--- Assignment5/api/main.py
+++ Assignment5/api/main.py
@@ -2,7 +2,7 @@ from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
 
 from .models import models, schemas
-from .controllers import orders, sandwiches
+from .controllers import orders, sandwiches, recipes
 from .dependencies.database import engine, get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -80,6 +80,36 @@ def delete_one_order(sandwich_id: int, db: Session = Depends(get_db)):
     return sandwiches.delete(db=db, sandwich_id=sandwich_id)
 
 
-##endpoints for Sandwich table##
+##endpoints for recipe table##
+@app.post("/recipes/", response_model=schemas.Recipe, tags=["Recipes"])
+def create_recipe(recipe: schemas.RecipeCreate, db: Session = Depends(get_db)):
+    return recipes.create_recipe(db=db, recipe=recipe)
 
 
+@app.get("/recipes/", response_model=list[schemas.Recipe], tags=["Recipes"])
+def read_recipes(db: Session = Depends(get_db)):
+    return recipes.get_recipes(db)
+
+
+@app.get("/recipes/{recipe_id}", response_model=schemas.Recipe, tags=["Recipes"])
+def read_one_order(recipe_id: int, db: Session = Depends(get_db)):
+    recipe = recipes.get_one_recipe(db, recipe_id=recipe_id)
+    if recipe is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return recipe
+
+
+@app.put("/recipes/{recipe_id}", response_model=schemas.Recipe, tags=["Recipes"])
+def update_one_recipe(recipe_id: int, recipe: schemas.Recipe, db: Session = Depends(get_db)):
+    recipe_db = recipes.get_one_recipe(db, recipe_id=recipe_id)
+    if recipe_db is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return recipes.update(db=db, recipe=recipe, recipe_id=recipe_id)
+
+
+@app.delete("/recipes/{recipe_id}", tags=["Recipes"])
+def delete_one_recipe(recipe_id: int, db: Session = Depends(get_db)):
+    recipe = recipes.get_one_recipe(db, recipe_id=recipe_id)
+    if recipe is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return recipe.delete(db=db, recipe_id=recipe_id)
