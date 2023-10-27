3702e47edd02d0c89c64532ab5d59f0e3cc45a53
Micah Grafton <mgrafto1@uncc.edu>
2023-10-26 21:26:21 -0400

    feat: added CRUD operations functions for resources.py

--- Assignment5/api/controllers/resources.py
+++ Assignment5/api/controllers/resources.py
@@ -1,1 +1,37 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response, Depends
+from ..models import models, schemas
 
+
+def create_resource(db: Session, resource):
+    db_resource = models.Resource(
+        customer_name=resource.customer_name,
+        description=resource.description
+    )
+    db.add(db_resource)
+    db.commit()
+    db.refresh(db_resource)
+    return db_resource
+
+
+def get_resources(db: Session):
+    return db.query(models.Resource).all()
+
+
+def get_one_resource(db: Session, resource_id):
+    return db.query(models.Resource).filter(models.Resource.id == resource_id).first()
+
+
+def update(db: Session, resource_id, resource):
+    db_resource = db.query(models.Resource).filter(models.Resource.id == resource_id)
+    update_data = resource.dict(exclude_unset=True)
+    db_resource.update(update_data, synchronize_session=False)
+    db.commit()
+    return db_resource.first()
+
+
+def delete(db: Session, db_resource):
+    db_resource = db.query(models.Resource).filter(models.Resource.id == db_resource)
+    db_resource.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
